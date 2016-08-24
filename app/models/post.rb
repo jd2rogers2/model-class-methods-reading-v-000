ecdd7cf21e3de99a1d3998096d1ed8c90c294121
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-24 09:53:19 -0400

    donzo

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,17 @@ class Post < ActiveRecord::Base
   belongs_to :author
 
   #put new code here
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.by_author(author_inst)
+    where(author: author_inst)
+  end
 
   private
 
